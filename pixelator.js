cef52866852dbaff4ddccd08c66f4876384c9747
LeeryanK <kylejlin.us@gmail.com>
2016-06-19 23:36:18 -0700

    Update pixelator.js

--- pixelator.js
+++ pixelator.js
@@ -1,37 +1,43 @@
-function Pixelator(image) {
-    var canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        width = image.width,
-        height = image.height,
-        imageData;
-    
-    this.width = width;
-    this.height = height;
-    
-    canvas.width = width;
-    canvas.height = height;
-    
-    ctx.drawImage(image, 0, 0, width, height);
-    
-    imageData = ctx.getImageData(0, 0, width, height);
-    this.pixels = new RGBAPixelCollection(imageData);
-}
+(function () {
+    function Pixelator(image) {
+        var canvas = document.createElement('canvas'),
+            ctx = canvas.getContext('2d'),
+            width = image.width,
+            height = image.height,
+            imageData;
+        
+        this.width = width;
+        this.height = height;
+        
+        canvas.width = width;
+        canvas.height = height;
+        
+        ctx.drawImage(image, 0, 0, width, height);
+        
+        imageData = ctx.getImageData(0, 0, width, height);
+        this.pixelCollection = new RGBAPixelCollection(imageData);
+    }
 
-Pixelator.prototype.pixelize = function (pixelWidth, pixelHeight) {
-    if (typeof pixelWidth !== 'number' || pixelWidth <= 0) {
-        throw new TypeError('The pixelWidth argument (arguments[0]) must be a non-zero number!');
-    }
-    if (typeof pixelHeight !== 'number' || pixelHeight <= 0) {
-        throw new TypeError('The pixelHeight argument (arguments[1]) must be a non-zero number!');
-    }
-    
-    var pixels = this.pixels,
-        length = pixels.length,
-        i;
-    
-    for (i = 0; i < length; i++) {
-        var pixel = pixels[i];
+    Pixelator.prototype.pixelize = function (pixelWidth, pixelHeight) {
+        if (typeof pixelWidth !== 'number' || pixelWidth <= 0) {
+            throw new TypeError('The pixelWidth argument (arguments[0]) must be a non-zero number!');
+        }
+        if (typeof pixelHeight !== 'number' || pixelHeight <= 0) {
+            throw new TypeError('The pixelHeight argument (arguments[1]) must be a non-zero number!');
+        }
         
+        pixelWidth = Math.floor(pixelWidth);
+        pixelHeight = Math.floor(pixelHeight);
+    
+        var pixels = this.pixelCollection.pixels,
+            length = pixels.length,
+            data = new Uint8ClampedArray(this.pixelCollection.width * this.pixelCollection.height * 4),
+            i;
         
-    }
-};
+        for (i = 0; i < length; i++) {
+            var pixel = pixels[i];
+            
+            
+        }
+    };
+})();
