f4bd186994a45e04a40c10ff048890f68e2586ad
Kyle Lin <kylejlin.us@gmail.com>
2016-07-07 22:37:17 -0700

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -47,6 +47,9 @@ var demo = (function() {
                 afterImgDataURL = pixelator.pixelate((widthInput.value | 0) || 10, (heightInput.value | 0) || 10).canvas.toDataURL('image/png', 1);
                 afterImg.src = afterImgDataURL;
                 
+                downloadLink.src = afterImgDataURL;
+                downloadLink.download = '(pixelated) ' + file.name;
+                
                 show(outputContainer);
                 
                 pixelations.push(new Pixelation(pixelator, dataURL, afterImgDataURL));
