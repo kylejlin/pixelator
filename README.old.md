9a529bf76b96aa9e714336d3f79f408a00aa65c8
Kyle Lin <kylejlin.us@gmail.com>
2019-08-09 17:30:55 -0700

    Rewrite html (in jsx)

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# pixelator
+A webpage that can pixelize images for you.
+It's basic algorithm is to divide up the image into sections and fill the sections with the average color of that section.
---
+++ src/components/Step.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Step.css";
+
+export default function Step({
+  number,
+  instructions,
+  children
+}: Props): React.ReactElement {
+  return (
+    <div className="Step">
+      <div className="StepDescription">
+        <span className="StepNumber">Step {number}: </span>
+        <span className="StepInstructions">{instructions}</span>
+      </div>
+
+      {children}
+    </div>
+  );
+}
+
+interface Props {
+  number: number;
+  instructions: string;
+  children: React.ReactNode;
+}
