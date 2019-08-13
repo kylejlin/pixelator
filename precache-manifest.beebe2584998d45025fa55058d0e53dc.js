38bd6169dc2eecb140aeaf3de2a3ccaa46a66873
Kyle Lin <kylejlin.us@gmail.com>
2019-08-12 21:05:21 -0700

    Updates

---
+++ precache-manifest.beebe2584998d45025fa55058d0e53dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef40c2ab7d0ccc83184d27ff1b15d5e",
+    "url": "/pixelator/index.html"
+  },
+  {
+    "revision": "8345086db8247a838e16",
+    "url": "/pixelator/static/css/main.6df2cc7e.chunk.css"
+  },
+  {
+    "revision": "db0c09f0f457d8d1bbe1",
+    "url": "/pixelator/static/js/2.23d97829.chunk.js"
+  },
+  {
+    "revision": "8345086db8247a838e16",
+    "url": "/pixelator/static/js/main.b519eac7.chunk.js"
+  },
+  {
+    "revision": "e4bce6653463d08f8e74",
+    "url": "/pixelator/static/js/runtime~main.1bf6f51b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a1063bdf9480b17f44639cd7a311132.js"
+  "/pixelator/precache-manifest.beebe2584998d45025fa55058d0e53dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pixelator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
